01751e2e7e3eb4838128e9a8ccc5946db3276841
bkeraj <62700770+bkeraj@users.noreply.github.com>
2020-06-24 23:11:32 +0200

    Add files via upload

---
+++ Code/doctor/comnpages/side.php
@@ -0,0 +1,60 @@
+ <aside>
+          <div id="sidebar"  class="nav-collapse ">
+              <!-- sidebar menu start-->
+              <ul class="sidebar-menu">                
+                  <li class="active">
+                      <a class="" href="index.php">
+                          <i class="icon_house_alt"></i>
+                          <span>Dashboard</span>
+                      </a>
+                  </li>
+				  
+				 
+				  
+				  
+				  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_profile"></i>
+                          <span>Appointment</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          
+                          <li><a class="" href="admin_doctor_app.php">View Appointment</a></li>
+                          <li><a class="" href="admin_doctor_app_visited.php">Add Appointment</a></li>
+                          <li><a class="" href="admin_doctor_app_cancel.php">Cancel Appointment</a></li>
+
+                          
+                      </ul>
+                  </li> 
+         <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_profile"></i>
+                          <span>Profile</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="./doctor_profile_view.php">View Profile</a></li>                          
+                          <li><a class="" href="./admin_Doctors_update.php">Update Profile</a></li>
+                          <li><a class="" href="./changePassword.php">Change Password</a></li>
+                      </ul>
+                  </li>
+
+                  <li class="sub-menu">
+                      <a href="javascript:;" class="">
+                          <i class="icon_profile"></i>
+                          <span>Patients</span>
+                          <span class="menu-arrow arrow_carrot-right"></span>
+                      </a>
+                      <ul class="sub">
+                          <li><a class="" href="./doctor_profile_view.php">My patients</a></li>
+                          <li><a class="" href="./admin_Doctors_update.php">Search patient</a></li>
+                      </ul>
+                  </li>
+
+
+
+              </ul>
+          </div>
+      </aside>
+     
