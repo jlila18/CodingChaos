aee536bed8c75eae95547b6678617144cd5bded7
bkeraj <62700770+bkeraj@users.noreply.github.com>
2020-06-24 22:42:31 +0200

    Add files via upload

---
+++ Code/comnpages/header.php
@@ -0,0 +1,71 @@
+<section class="page_topline cs main_color2 table_section table_section_sm section_padding_top_5 section_padding_bottom_5">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-8 text-center text-sm-left">
+							<div> <i class="fa fa-clock-o rightpadding_5" aria-hidden="true"></i> Opening Hours: Mon - Sat 10.00 - 20.00 </div>
+						</div>
+						<div class="col-sm-4 text-center text-sm-right greylinks"> <span class="rightpadding_10">Follow Us:</span> <a class="social-icon color-icon soc-facebook" href="#" title="Facebook"></a> <a class="social-icon color-icon soc-twitter" href="#" title="Twitter"></a> <a class="social-icon color-icon soc-google"
+							    href="#" title="Twitter"></a> <a class="social-icon color-icon soc-linkedin" href="#" title="Twitter"></a> <a class="social-icon color-icon soc-youtube" href="#" title="Youtube"></a> </div>
+					</div>
+				</div>
+			</section>
+			<section class="page_toplogo table_section table_section_md ls section_padding_top_25 section_padding_bottom_25">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 text-center text-md-left"> <a href="./" class="logo top_logo">
+                    <img src="images/our logo.png" alt="">
+                </a> </div>
+						<div class="col-md-9 text-center text-md-right">
+							<div class="inline-teasers-wrap">
+								<div class="teaser small-teaser media">
+									<div class="media-left media-middle">
+										<div class="teaser_icon size_small border_icon highlight2 rounded"> <i class="rt-icon2-pen"></i> </div>
+									</div>
+									<div class="media-body media-middle">
+										<h4>0 (355) 42 484177</h4>
+										<p class="greylinks fontsize_12"> <a href="mailto:e-dentclinic@gmail.com">e-dentclinic@gmail.com</a> </p>
+									</div>
+								</div>
+								<div class="teaser small-teaser media">
+									<div class="media-left media-middle">
+										<div class="teaser_icon size_small border_icon highlight2 rounded"> <i class="rt-icon2-map-pin"></i> </div>
+									</div>
+									<div class="media-body media-middle">
+										<h4>Dritan Hoxha Street</h4>
+										<p class="greylinks fontsize_12"> Tirane, Albania </p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<!-- Menue sections -->
+			<header class="page_header header_color toggler_left">
+				<div class="container">
+					<div class="row">
+						<div class="col-sm-12 display_table">
+							<div class="header_mainmenu display_table_cell">
+								<!-- main nav start -->
+								<nav class="mainmenu_wrapper">
+									<ul class="mainmenu nav sf-menu">
+										<li > <a href="index.php">Home</a></li>
+										 
+										 
+										<li > <a href="about.php">About us</a></li>
+										 
+										 <li > <a href="ourpatients.php">Our Patients</a></li>
+										<li > <a href="contact.php">Contact</a></li>
+                                        <li class="pull-right"> <a href="register.php">| Sign up </a></li>
+										<li class="pull-right"> <a href="Login.php">Login</a></li>
+
+									</ul>
+								</nav>
+								<span class="toggle_menu"><span></span></span>
+							</div>
+							 
+						</div>
+					</div>
+				</div>
+			</header>
+			
